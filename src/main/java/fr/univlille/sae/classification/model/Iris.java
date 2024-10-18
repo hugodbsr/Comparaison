ec577d77ee5df8160c0746daf99077b055001744
Matisse DEKEISER <matisse.dekeiser.etu@univ-lille.fr>
2024-10-18 09:49:21 +0200

    Ajout chargement des données CSV

--- src/main/java/fr/univlille/sae/classification/model/Iris.java
+++ src/main/java/fr/univlille/sae/classification/model/Iris.java
@@ -1,20 +1,25 @@
 package fr.univlille.sae.classification.model;
 
 import java.util.Set;
+import com.opencsv.bean.*;
 
 public class Iris extends LoadableData{
 
-
+    @CsvBindByName(column = "sepal.width")
     private double sepalWidth;
+    @CsvBindByName(column = "sepal.length")
     private double sepalLength;
+    @CsvBindByName(column = "petal.width")
     private double petalWidth;
+    @CsvBindByName(column = "petal.length")
     private double petalLength;
-
-
-
 
     public Iris(double sepalWidth, double sepalLength, double petalWidth, double petalLength) {
         this(sepalWidth, sepalLength, petalWidth, petalLength, "undefined");
+    }
+
+    public Iris() {
+        //
     }
 
     public Iris(double sepalWidth, double sepalLength, double petalWidth, double petalLength, String classification) {
@@ -24,9 +29,6 @@ public class Iris extends LoadableData{
         this.petalWidth = petalWidth;
         this.petalLength = petalLength;
     }
-
-
-
 
     public double getSepalWidth() {
         return sepalWidth;
@@ -42,5 +44,15 @@ public class Iris extends LoadableData{
 
     public double getPetalLength() {
         return petalLength;
+    }
+
+    @Override
+    public String toString() {
+        return "Iris{" +
+                "sepalWidth=" + sepalWidth +
+                ", sepalLength=" + sepalLength +
+                ", petalWidth=" + petalWidth +
+                ", petalLength=" + petalLength +
+                '}';
     }
 }
