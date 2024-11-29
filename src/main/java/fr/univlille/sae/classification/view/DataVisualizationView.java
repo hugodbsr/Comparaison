55af0193ccd3c9bcec51c8cc549d26bcb092cbaf
Hugo Desmons <hugo.desmons.etu@univ-lille.fr>
2024-11-29 10:08:21 +0100

    Refacto

--- src/main/java/fr/univlille/sae/classification/view/DataVisualizationView.java
+++ src/main/java/fr/univlille/sae/classification/view/DataVisualizationView.java
@@ -5,9 +5,16 @@ import fr.univlille.sae.classification.model.ClassificationModel;
 import fr.univlille.sae.classification.model.LoadableData;
 import fr.univlille.sae.classification.utils.Observable;
 import fr.univlille.sae.classification.utils.ViewUtil;
+import javafx.scene.Node;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.layout.HBox;
 import javafx.scene.shape.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Classe abstraite représentant une vue de visualisation des données.
@@ -23,10 +30,16 @@ public abstract class DataVisualizationView {
     protected String actualY;
     protected ScatterChart scatterChart;
     private MainStageController controller;
+
+    private Map<String, ScatterChart.Series<Double, Double>> serieList;
+    public ClassificationModel model;
     /**
      * Constructeur par défaut.
      */
-    protected DataVisualizationView() {}
+    protected DataVisualizationView(ClassificationModel model) {
+        this.serieList = new HashMap<String, ScatterChart.Series<Double, Double>>();
+        this.model = model;
+    }
 
     /**
      * Renvoie le nom de l'axe X actuel.
@@ -74,17 +87,97 @@ public abstract class DataVisualizationView {
     public abstract void reload();
 
 
-    /**
-     * Met à jour l'affichage en ajoutant un nouveau point de données.
-     * @param observable modèle observé.
-     * @param data point de données à ajouter.
-     */
+
+    public void update(Observable observable) {
+        try {
+            if (scatterChart == null || !(observable instanceof ClassificationModel)) {
+                System.err.println("Erreur de mise à jour.");
+                return;
+            }
+
+            scatterChart.getData().clear();
+            serieList.clear();
+
+            if (actualX == null && actualY == null) {
+                controller.setAxesSelected("Aucuns axes sélectionnés");
+            } else {
+                controller.setAxesSelected("");
+                controller.setAxesSelectedDisable();
+
+                List<LoadableData> points = new ArrayList<>(model.getDatas());
+                points.addAll(model.getDataToClass().keySet());
+                for (LoadableData data : points) {
+                    Object xValue = data.getAttributesNames().get(actualX);
+                    Object yValue = data.getAttributesNames().get(actualY);
+
+
+
+                    double x = 0;
+                    if(xValue instanceof Number) {
+                        x = ((Number) xValue).doubleValue();
+                    }
+                    double y = 0;
+                    if(yValue instanceof Number) {
+                        y = ((Number) yValue).doubleValue();
+                    }
+                    /**
+                     Double x = 0.0;
+                     if (xValue instanceof Integer) {
+                     x = ((Integer) xValue).doubleValue();
+                     } else if (xValue instanceof Double) {
+                     x = (Double) xValue;
+                     }
+
+                     Double y = 0.0;
+                     if (yValue instanceof Integer) {
+                     y = ((Integer) yValue).doubleValue();
+                     } else if (yValue instanceof Double) {
+                     y = (Double) yValue;
+                     }
+                     **/
+                    ScatterChart.Data<Double, Double> dataPoint = new ScatterChart.Data<>(x, y);
+
+                    Node nodePoint = ViewUtil.getForm(data, new Circle(5), controller);
+
+                    ScatterChart.Series<Double, Double> editSerie = serieList.get(data.getClassification());
+                    if(editSerie == null){
+                        editSerie = new ScatterChart.Series<Double, Double>();
+                    }
+                    if(data.getClassification().equals("undefined") || model.getDataToClass().containsKey(data)) {
+                        nodePoint = ViewUtil.getForm(data, new Rectangle(10,10), controller);
+                    }
+
+                    dataPoint.setNode(nodePoint);
+                    editSerie.getData().add(dataPoint);
+                    serieList.put(data.getClassification(), editSerie);
+                }
+
+                for(String serie : serieList.keySet()) {
+                    serieList.get(serie).setName(serie);
+                }
+                scatterChart.getData().addAll(serieList.values());
+
+
+                HBox hBox = ViewUtil.loadLegend();
+                controller.loadLegend(hBox);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println("Erreur de mise à jour : " + e.getMessage());
+        }
+    }
+
+
     public void update(Observable observable, Object data) {
         try {
             if (scatterChart == null || !(observable instanceof ClassificationModel)) {
                 System.err.println("Erreur de mise à jour.");
                 return;
             }
+
+
+
             LoadableData newData = (LoadableData) data;
             if (actualX == null || actualY == null) {
                 controller.setAxesSelected("Aucuns axes sélectionnés");
@@ -104,15 +197,14 @@ public abstract class DataVisualizationView {
             );
 
             dataPoint.setNode(ViewUtil.getForm(newData, new Rectangle(10, 10), controller));
-
             if (!scatterChart.getData().isEmpty()) {
-                if (series4 == null) {
-                    series4 = new ScatterChart.Series<>();
-                }
                 series4.getData().add(dataPoint);
                 series4.setName("indéfini");
                 scatterChart.getData().add(series4);
             }
+
+
+            controller.loadLegend(ViewUtil.loadLegend());
         } catch (Exception e) {
             System.err.println("Erreur de mise à jour : " + e.getMessage());
         }
